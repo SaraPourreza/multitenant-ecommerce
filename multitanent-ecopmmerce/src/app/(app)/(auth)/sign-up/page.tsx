831be76410d5dfd5fb1f772aa01a1ab0c7846211
sara <sarahpourreza@yahoo.com>
2025-07-18 18:05:24 +0330

    09:auth states

--- multitanent-ecopmmerce/src/app/(app)/(auth)/sign-up/page.tsx
+++ multitanent-ecopmmerce/src/app/(app)/(auth)/sign-up/page.tsx
@@ -1,7 +1,13 @@
 import { SignUpView } from '@/modules/auth/ui/views/sign-up-view'
-import React from 'react'
+import { caller } from '@/trpc/server';
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+    const session = await caller.auth.session()
+  // agar login bashe dige nare sign up ya login
+    if (session.user){
+      redirect("/")
+    }
   return(
     <SignUpView />
   )
